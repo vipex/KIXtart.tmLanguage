bead73d32dfffea2bc04fb0521bee9cef4ad89fe
Luca Ravizzotti <vipex@vipexsoft.com>
2016-09-28 20:30:50 +0200

    fixed tests

--- tests/test.py
+++ tests/test.py
@@ -2,9 +2,9 @@ from xml.dom import minidom
 import json
 
 def test_tmlanguage_is_valid():
-    f = open('KIXtart.tmLanguage')
+    f = open('src/KIXtart.tmLanguage')
     minidom.parse(f)
     
 def test_sublime_build_is_valid():
-    f = open('KIXtart.sublime-build')
+    f = open('src/KIXtart.sublime-build')
     json.load(f)
